4b76781faf66719ba98ee419152ac3d0347117b1
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-27 12:55:42 +0530

    ADD. added welcome file in employee wage builder demo reposiritory

---
+++ src/empwagebuilderdemo/EmpWageBuilderDemo.java
@@ -0,0 +1,10 @@
+package empwagebuilderdemo;
+
+public class EmpWageBuilderDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to employye wage builder program:");
+	}
+
+}
