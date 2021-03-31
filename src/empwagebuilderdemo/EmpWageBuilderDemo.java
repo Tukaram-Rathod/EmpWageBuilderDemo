2fb6f5845df888a0ca753b82f9f8e8166eb4594f
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-31 13:48:30 +0530

    ADD. total number of working hour rechages

--- src/empwagebuilderdemo/EmpWageBuilderDemo.java
+++ src/empwagebuilderdemo/EmpWageBuilderDemo.java
@@ -5,20 +5,22 @@ public class EmpWageBuilderDemo {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
-			
+	public static final int MAX_HRS_IN_MONTH = 100;
+		
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to employye wage builder program:");
 		
 		//Variable
-		int empHrs = 0, empWage = 0,totalEmpWage=0;
-		
+		int empHrs = 0, empWage=0,totalEmpHrs = 0,totalEmpWage=0,totalWorkingdays =0;
 		
 		//computation
-		int empCheck =(int) Math.floor(Math.random() * 10 ) % 3;
 
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
-			
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && 
+				totalWorkingdays < NUM_OF_WORKING_DAYS) {
+
+			totalWorkingdays++;
+			int empCheck =(int) Math.floor(Math.random() * 10 ) % 3;
 			switch(empCheck) {
 				case IS_PART_TIME :
 					empHrs = 4;
@@ -28,14 +30,11 @@ public class EmpWageBuilderDemo {
 					break;
 				default :
 					empHrs = 0;
-			}
-		
-		//calculate daily wages
-			
-		empWage = empHrs *  EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage; 
-		System.out.println("Emp WAGE ==> " +empWage );
+			}			
+			totalEmpHrs += empHrs;	
+			System.out.println("Days#: " +totalWorkingdays+ "Emp Hrs :" + empHrs +":" );
 		}
+		totalEmpWage = totalEmpHrs *  EMP_RATE_PER_HOUR;
 		System.out.println("Total EmpWage ==> " +totalEmpWage);
 	}
 }
