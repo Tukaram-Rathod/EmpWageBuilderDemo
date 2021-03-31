e03fbf6fcccbaa8efd9865eb10457af68ce432a6
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-31 12:44:00 +0530

    ADD. Add Part time

--- src/empwagebuilderdemo/EmpWageBuilderDemo.java
+++ src/empwagebuilderdemo/EmpWageBuilderDemo.java
@@ -6,7 +6,8 @@ public class EmpWageBuilderDemo {
 		System.out.println("welcome to employye wage builder program:");
 		
 		//constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		//Variable
@@ -14,10 +15,11 @@ public class EmpWageBuilderDemo {
 		int empWage = 0;
 		
 		//computation
-		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		double empCheck = Math.floor(Math.random() * 10 ) % 3;
 		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
-			
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		
