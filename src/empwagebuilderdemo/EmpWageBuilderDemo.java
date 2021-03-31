8a10687e2854ae6dd457de2e184cdc8e8e439208
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-31 12:29:08 +0530

    ADD.added to check emp is present or not

--- src/empwagebuilderdemo/EmpWageBuilderDemo.java
+++ src/empwagebuilderdemo/EmpWageBuilderDemo.java
@@ -1,10 +1,16 @@
 package empwagebuilderdemo;
-
 public class EmpWageBuilderDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to employye wage builder program:");
-	}
 
+		int IS_PRESENT = 1;
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Employee Is Present"); 
+		else
+			System.out.println("Employee Is Absent");
+		
+	}
 }
