6814ade2f58d9306b67b77893fec7978246a7b47
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-31 12:39:38 +0530

    ADD. added calculate daily employee wages

--- src/empwagebuilderdemo/EmpWageBuilderDemo.java
+++ src/empwagebuilderdemo/EmpWageBuilderDemo.java
@@ -2,15 +2,30 @@ package empwagebuilderdemo;
 public class EmpWageBuilderDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to employye wage builder program:");
-
-		int IS_PRESENT = 1;
-		double empCheck = Math.floor(Math.random() * 10 ) % 2;
-		if (empCheck == IS_PRESENT)
-			System.out.println("Employee Is Present"); 
-		else
-			System.out.println("Employee Is Absent");
 		
-	}
+		System.out.println("welcome to employye wage builder program:");
+		
+		//constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+
+		//Variable
+		int empHrs = 0;
+		int empWage = 0;
+		
+		//computation
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+			
+		else
+			empHrs = 0;
+		
+		//calculate daily wages
+			
+		empWage = empHrs *  EMP_RATE_PER_HOUR;
+		System.out.println("Emp WAGE " +empWage );
+
+		}
+
 }
